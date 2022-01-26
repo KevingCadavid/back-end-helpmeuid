b9f45f1fc0d7a013e0ed575b94801e40a3f93fc7
KevingCadavid <you@example.com>
2022-01-26 00:41:59 -0500

    :sparkles: implementing businnes logic service & controller Delitos

--- src/main/java/co/edu/iudigital/app/dto/DelitoDto.java
+++ src/main/java/co/edu/iudigital/app/dto/DelitoDto.java
@@ -3,12 +3,30 @@ package co.edu.iudigital.app.dto;
 import lombok.Builder;
 import lombok.Data;
 
-@Data 
-@Builder 
+//@Data 
+//@Builder 
 public class DelitoDto {
 	
 	private Long id;
 	private String nombre;
 	private String descripcion;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 
 }
